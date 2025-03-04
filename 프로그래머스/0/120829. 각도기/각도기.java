ba536026810b029dcf2e26cffe8f645d8376e85f
Garam ANN <maragriver@gmail.com>
2025-03-04 16:24:09 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle > 0 && angle < 90) {
+            return 1;  // 예각
+        } else if (angle == 90) {
+            return 2;  // 직각
+        } else if (angle > 90 && angle < 180) {
+            return 3;  // 둔각
+        } else if (angle == 180) {
+            return 4;  // 평각
+        } 
+        return 0;  // 잘못된 입력 처리
+    }
+}
