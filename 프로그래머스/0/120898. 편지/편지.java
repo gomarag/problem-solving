bd68f16d9f577649040e9450701f21f488c56038
Garam ANN <maragriver@gmail.com>
2025-03-04 17:02:47 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        // int answer = 0;
+        // return answer;
+        int msgLength = message.length();
+        return msgLength * 2;
+    }
+}
